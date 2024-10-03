ce6bb458f6deff499617fc48140c3607e72782fd
kyujin <dullkim@naver.com>
2024-10-03 22:02:58 +0900

    quasar 글꼴 활용하기

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,12 +4,8 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
-</script>
+<script setup></script>
