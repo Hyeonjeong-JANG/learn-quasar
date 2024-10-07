f4b555a448776fa1a20b0d0793d85ebf159438ac
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-10-07 19:42:53 +0900

    퀘이사 기초(프로젝트 생성, 기본 세팅, Typography, Colors, Spacing, Helper Classes

---
+++ src/pages/ClassesVariables.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="q-pa-xl">
+    <section class="q-mb-xl">
+      <div class="text-h4">Shadow</div>
+      <q-separator class="q-my-md" />
+      <div class="q-gutter-y-sm">
+        <div class="shadow-1 q-pa-sm">shadow-1</div>
+        <div class="shadow-7 q-pa-sm">shadow-7</div>
+        <div class="shadow-24 q-pa-sm">shadow-24</div>
+      </div>
+    </section>
+    <section class="q-mb-xl">
+      <div class="text-h4">Visibility</div>
+      <q-separator class="q-my-md" />
+      <div class="ellipsis">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium
+        eaque provident, voluptates quasi eum, distinctio quis id facilis
+        impedit reiciendis, quisquam debitis! Laudantium quod consectetur
+        quibusdam illum enim neque accusantium.
+      </div>
+      <hr />
+      <div class="ellipsis-2-lines cursor-pointer">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium
+        eaque provident, voluptates quasi eum, distinctio quis id facilis
+        impedit reiciendis, quisquam debitis! Laudantium quod consectetur
+        quibusdam illum enim neque accusantium.
+      </div>
+      <hr />
+      <div class="ellipsis-3-lines">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium
+        eaque provident, voluptates quasi eum, distinctio quis id facilis
+        impedit reiciendis, quisquam debitis! Laudantium quod consectetur
+        quibusdam illum enim neque accusantium.
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,13 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: 'typography', component: () => import('pages/Typography.vue') },
+      { path: 'colors', component: () => import('pages/Colors.vue') },
+      { path: 'spacing', component: () => import('pages/Spacing.vue') },
+      { path: 'breakpoints', component: () => import('pages/Breakpoints.vue') },
+      {
+        path: 'classes-variables',
+        component: () => import('pages/ClassesVariables.vue'),
+      },
     ],
   },
 
