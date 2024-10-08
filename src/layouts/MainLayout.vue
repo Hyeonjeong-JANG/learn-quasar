233c8fbb9affb405d95390f6f0ae094f8d7050a0
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-10-08 14:11:08 +0900

    인스타그램 프로필 페이지

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,6 +16,11 @@
         <q-btn stretch flat label="Sub Layout" to="/sub" />
         <q-btn flat round dense :icon="darkModeIcon" @click="toggleDarkMode" />
         <!-- dark_mode -->
+        <q-btn round size="sm" class="q-ml-md" to="/profile">
+          <q-avatar>
+            <img src="/insta-logo.png" />
+          </q-avatar>
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -79,12 +84,12 @@ const linksList = [
     icon: 'school',
     to: '/flex-grid-1',
   },
-  {
-    title: 'Flex Grid 2',
-    caption: 'quasar.dev',
-    icon: 'school',
-    to: '/flex-grid-2',
-  },
+  // {
+  //   title: 'Flex Grid 2',
+  //   caption: 'quasar.dev',
+  //   icon: 'school',
+  //   to: '/flex-grid-2',
+  // },
 ];
 </script>
 <script setup>
--- src/pages/profile/ProfilePage.vue
+++ src/pages/profile/ProfilePage.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="col-12 col-sm-grow column justify-between">
         <div class="row items-center">
-          <span class="text-h6 text-weight-bold">helinlee.gram</span>
+          <span class="text-h6 text-weight-bold">ophen28</span>
           <q-space />
           <div class="q-gutter-x-sm">
             <q-btn outline color="secondary" label="프로필 편집1" />
@@ -42,28 +42,47 @@
       </div>
     </section>
     <section class="q-mt-xl">
-      <div class="row q-col-gutter-md">
-        <div
-          v-for="n in 18"
-          :key="n"
-          class="col-12 col-sm-6 col-md-46 col-lg-3"
-        >
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" ratio="1">
-              <div
-                class="absolute-bottom text-subtitle2 flex flex-center full-height"
-              >
-                Title
-              </div>
-            </q-img>
-          </q-card>
-        </div>
-      </div>
+      <q-tabs
+        class="q-mb-lg"
+        v-model="tab"
+        inline-label
+        switch-indicator
+        indicator-color="primary"
+      >
+        <!-- <q-tab name="mails" icon="mail" label="게시글" />
+        <q-tab name="alarms" icon="alarm" label="저장됨" />
+        <q-tab name="movies" icon="movie" label="태그됨" /> -->
+        <q-route-tab
+          :ripple="false"
+          icon="edit"
+          to="/profile"
+          label="게시글"
+          exact
+        />
+        <q-route-tab
+          :ripple="false"
+          icon="bookmark_border"
+          to="/profile/saved"
+          label="저장됨"
+          exact
+        />
+        <q-route-tab
+          :ripple="false"
+          icon="local_offer"
+          to="/profile/tagged"
+          label="태그됨"
+          exact
+        />
+      </q-tabs>
+      <router-view />
     </section>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const tab = ref('mails');
+</script>
 
 <style lang="scss" scoped>
 .q-page {
