efe210557e0afefda5c661ce2b560688a5498135
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-10-08 11:24:26 +0900

    인스타그램

--- src/pages/FlexGrid2.vue
+++ src/pages/FlexGrid2.vue
@@ -1,12 +1,72 @@
 <template>
-  <q-page class="q-pa-xl">
-    <section class="q-mb-xl">
-      <div class="text-h4">Flex Grid 2</div>
-      <q-separator class="q-my-md" />
+  <q-page padding>
+    <section class="row q-col-gutter-x-lg q-py-lg">
+      <div class="col-12 col-sm-auto q-py-md flex flex-center">
+        <q-avatar size="150px">
+          <q-img src="/insta-logo.png" />
+        </q-avatar>
+      </div>
+      <div class="col-12 col-sm-grow column justify-between">
+        <div class="row items-center">
+          <span class="text-h6 text-weight-bold">helinlee.gram</span>
+          <q-space />
+          <div class="q-gutter-x-sm">
+            <q-btn outline color="secondary" label="프로필 편집1" />
+            <q-btn outline color="secondary" label="프로필 편집2" />
+            <q-btn outline color="secondary" label="프로필 편집3" />
+            <q-btn flat color="secondary" icon="more_horiz" />
+          </div>
+        </div>
+        <div class="q-gutter-x-lg">
+          <span>게시물 1895</span>
+          <span>팔로워 138.3만</span>
+          <span>팔로우 456</span>
+        </div>
+        <div>
+          <div>주우재</div>
+          <div>Da Capo</div>
+          <div>youtu.be/P3gN6NCTBqA?si=R6qpId8l5Zob7MgR</div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="row no-wrap scroll q-gutter-x-xl">
+        <div class="col-auto" v-for="n in 15" :key="n">
+          <article>
+            <q-avatar size="77px">
+              <q-img src="/insta-logo.png" />
+            </q-avatar>
+            <div class="text-center q-mt-sm">제목</div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section class="q-mt-xl">
+      <div class="row q-col-gutter-md">
+        <div
+          v-for="n in 18"
+          :key="n"
+          class="col-12 col-sm-6 col-md-46 col-lg-3"
+        >
+          <q-card class="my-card">
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" ratio="1">
+              <div
+                class="absolute-bottom text-subtitle2 flex flex-center full-height"
+              >
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+      </div>
     </section>
   </q-page>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.q-page {
+  background-color: #fafafa;
+}
+</style>
